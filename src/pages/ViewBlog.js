659dcec32ff734239a43ae67169697ed64f5b2e6
Arjun Lubana <arjunlubana562@gmail.com>
2022-02-28 14:47:19 +0300

    Styled the View blog page

--- src/pages/ViewBlog.js
+++ src/pages/ViewBlog.js
@@ -1,29 +1,32 @@
 import { Link, useParams } from "react-router-dom";
+import { IconContext } from "react-icons";
+import { IoCreateOutline, IoTrashOutline } from "react-icons/io5";
 import { BlogEditor } from "../components";
 
-export default function ViewBlog({
-  blog,
-  deleteBlog,
-}) {
+export default function ViewBlog({ blog, deleteBlog }) {
   const params = useParams();
 
   return (
-    <div className="container w-100">
-      <Link className="btn btn-info" to={`/blog/${params.blogId}/edit`}>
-        {" "}
-        Edit{" "}
-      </Link>{" "}
-      <button className="btn btn-info" onClick={deleteBlog}>
-        {" "}
-        Delete{" "}
-      </button>{" "}
-        <BlogEditor
-          blog={blog}
-          readOnly={true}
-        />
-      <div>
-        <h4> Comments </h4> <h4> Related Content </h4>{" "}
-      </div>{" "}
+    <div className="container-fluid">
+      <IconContext.Provider value={{ size: "2rem" }}>
+        <div className="d-flex justify-content-center">
+          <Link
+            className="btn btn-outline-info m-1 fs-5 d-flex align-items-center"
+            to={`/blog/${params.blogId}/edit`}
+          >
+            <IoCreateOutline />
+            <span>Edit</span>
+          </Link>
+          <button
+            className="btn btn-danger m-1 fs-5 d-flex align-items-center"
+            onClick={deleteBlog}
+          >
+            <IoTrashOutline />
+            <span>Delete</span>
+          </button>
+        </div>
+      </IconContext.Provider>
+      <BlogEditor blog={blog} readOnly={true} />
     </div>
   );
 }
