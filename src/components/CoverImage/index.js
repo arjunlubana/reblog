d0a5b69de1776de4bb65971c8ab393c7f71ab27a
Arjun Lubana <arjunlubana562@gmail.com>
2022-03-04 12:16:47 +0300

    Show the blog cover image on the edit page.

--- src/components/CoverImage/index.js
+++ src/components/CoverImage/index.js
@@ -5,7 +5,7 @@ import "filepond/dist/filepond.min.css";
 import FilePondPluginImagePreview from "filepond-plugin-image-preview";
 import FilePondPluginImageResize from "filepond-plugin-image-resize";
 import FilePondPluginImageCrop from "filepond-plugin-image-crop";
-import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type"
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
 import FilePondPluginImageTransform from "filepond-plugin-image-transform";
 import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
 import "./styles.css";
@@ -19,12 +19,19 @@ registerPlugin(
 );
 
 export default function CoverImage({
+  blogId,
   coverImage,
   setCoverImage,
   blogUpdate,
   setBlogUpdate,
   readOnly,
 }) {
+  const [files, setFiles] = useState(
+    coverImage
+      ? [{ source: `${coverImage.filename}`, options: { type: "local" } }]
+      : []
+  );
+
   return readOnly ? (
     coverImage ? (
       <img
@@ -39,8 +46,9 @@ export default function CoverImage({
     <FilePond
       allowMultiple={false}
       acceptedFileTypes={["image/*"]}
-      labelIdle='
-      <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+      files={files}
+      onupdatefiles={setFiles}
+      labelIdle={`<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
         width="64px" height="64px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
         <g>
         <path d="M368,224c26.5,0,48-21.5,48-48c0-26.5-21.5-48-48-48c-26.5,0-48,21.5-48,48C320,202.5,341.5,224,368,224z"/>
@@ -49,7 +57,7 @@ export default function CoverImage({
         c-4.3,0-8.2-1.6-11-4.1c-1-0.9-2.8-2.6-4.3-4.1L224,215.3c-4-4.6-10-7.5-16.7-7.5c-6.7,0-12.9,3.3-16.8,7.8L64,368.2V107.7
         c1-6.8,6.3-11.7,13.1-11.7h357.7c6.9,0,12.5,5.1,12.9,12l0.3,260.4L348.9,261.7z"/>
         </g>
-       </svg>'
+       </svg>`}
       stylePanelLayout="compact"
       stylePanelAspectRatio="16:9"
       imageCropAspectRatio="16:9"
@@ -60,6 +68,12 @@ export default function CoverImage({
           blogUpdate.append("cover", output);
         }
         setBlogUpdate(blogUpdate);
+      }}
+      server={{
+        url: `${api_url}`,
+        process: null,
+        load: "/",
+        patch: `/${blogId}`,
       }}
       credits={false}
     />
