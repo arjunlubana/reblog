c7d00a42b7460a8a0014054bbdc5de5834af4c06
Arjun Lubana <arjunlubana562@gmail.com>
2022-03-04 19:18:41 +0300

    Show preview text and title on the blog card

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { convertFromRaw } from "draft-js";
 import { Spinner } from "../components";
 import api_url from "../lib/api-url";
 import { getBlogs } from "../lib/blog-crud";
 
 export default function Blogs({ blogs, setBlogs }) {
-
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -21,20 +21,24 @@ export default function Blogs({ blogs, setBlogs }) {
       <Spinner />
     </div>
   ) : (
-    <div className="container-fluid w-75 mx-auto" id="blogs">
+    <div className="container-fluid" id="blogs">
       <h1 className="text-center">Blogs</h1>
       {blogs.map((blog) => (
-        <div className="card d-flex flex-row" key={blog.id}>
+        <div
+          className="card d-flex flex-column w-50 h-50 mx-auto"
+          key={blog.id}
+        >
           <img
             src={blog.cover ? `${api_url}/${blog.cover.filename}` : ""}
-            className="card-image"
-            alt="..."
+            className=" w-100"
+            alt="Cover image"
           />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
+            <h5 className="card-title">
+              {convertFromRaw(blog.title).getFirstBlock().getText()}
+            </h5>
             <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              {convertFromRaw(blog.body).getFirstBlock().getText()}
             </p>
             <Link to={`/blog/${blog.id}`} className="stretched-link" />
           </div>
