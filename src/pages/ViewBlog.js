795a3731ef6cce1e71ff3303831dbd38b51c043f
Arjun Lubana <arjunlubana562@gmail.com>
2022-03-05 12:25:34 +0300

    Remove publish and delete functions in view blog

--- src/pages/ViewBlog.js
+++ src/pages/ViewBlog.js
@@ -1,13 +1,9 @@
 import { Link, useParams } from "react-router-dom";
 import { IconContext } from "react-icons";
-import {
-  IoCreateOutline,
-  IoTrashOutline,
-  IoCloudUploadOutline,
-} from "react-icons/io5";
+import { IoCreateOutline } from "react-icons/io5";
 import { BlogEditor } from "../components";
 
-export default function ViewBlog({ blog, deleteBlog, publishBlog }) {
+export default function ViewBlog({ blog }) {
   const params = useParams();
 
   return (
@@ -21,22 +17,6 @@ export default function ViewBlog({ blog, deleteBlog, publishBlog }) {
             <IoCreateOutline />
             <span>Edit</span>
           </Link>
-
-          <button
-            className="btn btn-success m-1 fs-6 d-flex align-items-center"
-            onClick={publishBlog}
-          >
-            <IoCloudUploadOutline />
-            <span>Publish</span>
-          </button>
-
-          <button
-            className="btn btn-danger m-1 fs-6 d-flex align-items-center"
-            onClick={deleteBlog}
-          >
-            <IoTrashOutline />
-            <span>Delete</span>
-          </button>
         </div>
       </IconContext.Provider>
       <BlogEditor blog={blog} readOnly={true} />
