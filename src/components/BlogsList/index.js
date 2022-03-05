1f7e75a276017937ca3e48ab63b13f260a1278eb
Arjun Lubana <arjunlubana562@gmail.com>
2022-03-05 20:02:51 +0300

    Reformating code with prettier

--- src/components/BlogsList/index.js
+++ src/components/BlogsList/index.js
@@ -2,46 +2,31 @@ import { Link } from "react-router-dom";
 import { convertFromRaw } from "draft-js";
 import { Spinner, EmptyBlogs } from "../";
 import api_url from "../../lib/api-url";
-import "./styles.css"
+import "./styles.css";
 
-export default function BlogsList({blogs}) {
-
-	return blogs.length === 0 ? (
-		<EmptyBlogs />
-	) : (
-		<div className="mt-5" id="blogs">
-			{blogs.map((blog) => (
-				<div
-					className="card"
-					key={blog.id}
-				>
-					<img
-						src={
-							blog.cover
-								? `${api_url}/${blog.cover.filename}`
-								: ""
-						}
-						className="card-img-top"
-						alt="Blog Cover"
-					/>
-					<div className="card-body">
-						<h5 className="card-title text-center">
-							{convertFromRaw(blog.title)
-								.getFirstBlock()
-								.getText()}
-						</h5>
-						<p className="card-text">
-							{convertFromRaw(blog.body)
-								.getFirstBlock()
-								.getText()}
-						</p>
-						<Link
-							to={`/blog/${blog.id}`}
-							className="stretched-link"
-						/>
-					</div>
-				</div>
-			))}
-		</div>
-	);
+export default function BlogsList({ blogs }) {
+  return blogs.length === 0 ? (
+    <EmptyBlogs />
+  ) : (
+    <div className="mt-5" id="blogs">
+      {blogs.map((blog) => (
+        <div className="card" key={blog.id}>
+          <img
+            src={blog.cover ? `${api_url}/${blog.cover.filename}` : ""}
+            className="card-img-top"
+            alt="Blog Cover"
+          />
+          <div className="card-body">
+            <h5 className="card-title text-center">
+              {convertFromRaw(blog.title).getFirstBlock().getText()}
+            </h5>
+            <p className="card-text">
+              {convertFromRaw(blog.body).getFirstBlock().getText()}
+            </p>
+            <Link to={`/blog/${blog.id}`} className="stretched-link" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
