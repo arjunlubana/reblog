d94565b5316881d060106744ae924ccd85d5b3e2
Arjun Lubana <arjunlubana562@gmail.com>
2022-03-07 21:49:09 +0300

    Convert blog title & body to raw before updating

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -7,7 +7,6 @@ import { Spinner } from "components";
 export default function Blog({ render, blogs, setBlogs }) {
   const params = useParams();
   const navigate = useNavigate();
-  const [blogUpdate, setBlogUpdate] = useState(new FormData());
   const [blog, setBlog] = useState({});
   const [isLoading, setIsLoading] = useState(true);
 
@@ -27,24 +26,27 @@ export default function Blog({ render, blogs, setBlogs }) {
         title: convertToRaw(EditorState.createEmpty().getCurrentContent()),
         body: convertToRaw(EditorState.createEmpty().getCurrentContent()),
       };
-      createBlog(blog_data).then((response) => {
+      createBlog(blog_data).then((data) => {
         setBlog({
-          ...response,
-          title: EditorState.createWithContent(convertFromRaw(response.title)),
-          body: EditorState.createWithContent(convertFromRaw(response.body)),
+          ...data,
+          title: EditorState.createWithContent(convertFromRaw(data.title)),
+          body: EditorState.createWithContent(convertFromRaw(data.body)),
         });
         setIsLoading(false);
       });
     }
   }, []);
-
   // Update blog
   const update_blog = (id) => {
-    updateBlog(id, blog);
+    updateBlog(id, {
+      ...blog,
+      title: convertToRaw(blog.title.getCurrentContent()),
+      body: convertToRaw(blog.body.getCurrentContent()),
+    });
   };
 
   const publish_blog = (id) => {
-    updateBlog(id, {...blog, publish: true});
+    updateBlog(id, { ...blog, publish: true });
   };
 
   // Delete a blog from the UI and Backend
@@ -53,8 +55,8 @@ export default function Blog({ render, blogs, setBlogs }) {
     setBlogs(blogs.filter((blog) => blog.id !== id));
     navigate("/", { replace: true });
   };
-  // The render prop renders either an edit blog or a view blog.
-  // Title and Blog states, delete and update blog functions are passed to the rendered components
+
+
   return isLoading ? (
     <div className="vh-100">
       <Spinner />
