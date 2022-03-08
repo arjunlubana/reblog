099869d1833a5d328caac8e90036a7b01477c117
Arjun Lubana <arjunlubana562@gmail.com>
2022-03-08 12:24:46 +0300

    Fix: title editor height smaller than placeholder height

--- src/components/BlogTitle/index.js
+++ src/components/BlogTitle/index.js
@@ -1,36 +1,27 @@
-import { useEffect } from "react";
 import { Editor, RichUtils } from "draft-js";
 import "draft-js/dist/Draft.css";
 
-export default function BlogTitle({
-  blog,
-  setBlog,
-  readOnly
-}) {
+export default function BlogTitle({ blog, setBlog, readOnly }) {
   const handleChange = (editorState) => {
     setBlog({ ...blog, title: editorState });
   };
 
-  useEffect(() => {
-    // Only toggle Title header style if the block is unstyled
-    if (
-      blog.title.getCurrentContent().getFirstBlock().getType() === "unstyled"
-    ) {
-      setBlog({
-        ...blog,
-        title: RichUtils.toggleBlockType(blog.title, "header-one")
-      });
-    }
-  }, [blog.title]);
+  // Only toggle Title header style if the block is unstyled
+  if (blog.title.getCurrentContent().getFirstBlock().getType() === "unstyled") {
+    setBlog({
+      ...blog,
+      title: RichUtils.toggleBlockType(blog.title, "header-one"),
+    });
+  }
 
   return (
-    <Editor
-      editorState={blog.title}
-      onChange={handleChange}
-      placeholder={<h1>Blog Title</h1>}
-      handleReturn={() => "handled"}
-      textAlignment="center"
-      readOnly={readOnly}
-    />
+      <Editor
+        editorState={blog.title}
+        onChange={handleChange}
+        placeholder={<h1>Blog Title</h1>}
+        handleReturn={() => "handled"}
+        textAlignment="center"
+        readOnly={readOnly}
+      />
   );
 }
