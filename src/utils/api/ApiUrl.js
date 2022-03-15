822fad6ae10ae5e09702cc10cbe0c845ca0a2b0a
Arjun Lubana <arjunlubana562@gmail.com>
2022-03-15 23:09:56 +0300

    Fix API uri to avoid trailing slashes

--- src/utils/api/ApiUrl.js
+++ src/utils/api/ApiUrl.js
@@ -3,8 +3,8 @@ export let BASE_URL;
 BASE_URL =
   process.env.NODE_ENV === "production"
     ? process.env.REACT_APP_API_URL
-    : (BASE_URL = "http://localhost:5000");
+    : (BASE_URL = "http://localhost:5000/");
 
-export const FILES_URI = "/files/"
-export const BLOGS_URI = "/blogs/"
-export const USER_URI = "/users"
+export const FILES_URI = "files/"
+export const BLOGS_URI = "blogs"
+export const USER_URI = "users"
