46cd466f6e816871b903da7ee6c7f5aabfe47c0d
Arjun Lubana <arjunlubana562@gmail.com>
2022-03-15 22:46:03 +0300

    Fix React warning on BlogTitle component

--- src/components/BlogTitle/index.js
+++ src/components/BlogTitle/index.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Editor, RichUtils } from "draft-js";
 import "draft-js/dist/Draft.css";
 
@@ -7,21 +8,25 @@ export default function BlogTitle({ blog, setBlog, readOnly }) {
   };
 
   // Only toggle Title header style if the block is unstyled
-  if (blog.title.getCurrentContent().getFirstBlock().getType() === "unstyled") {
-    setBlog({
-      ...blog,
-      title: RichUtils.toggleBlockType(blog.title, "header-one"),
-    });
-  }
+  useEffect(() => {
+    if (
+      blog.title.getCurrentContent().getFirstBlock().getType() === "unstyled"
+    ) {
+      setBlog({
+        ...blog,
+        title: RichUtils.toggleBlockType(blog.title, "header-one"),
+      });
+    }
+  }, [blog, setBlog]);
 
   return (
-      <Editor
-        editorState={blog.title}
-        onChange={handleChange}
-        placeholder={<h1>Blog Title</h1>}
-        handleReturn={() => "handled"}
-        textAlignment="center"
-        readOnly={readOnly}
-      />
+    <Editor
+      editorState={blog.title}
+      onChange={handleChange}
+      placeholder={<h1>Blog Title</h1>}
+      handleReturn={() => "handled"}
+      textAlignment="center"
+      readOnly={readOnly}
+    />
   );
 }
