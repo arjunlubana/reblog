3eb0d81b141f3955c975b8ba3f04170a3c59ce85
Arjun Lubana <arjunlubana562@gmail.com>
2022-05-29 11:27:27 +0300

    Remove useAuth0 unused vars

--- src/hooks/useBlog.jsx
+++ src/hooks/useBlog.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { useAuth0 } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 import { getEditorState, getRawEditorState } from "utils";
 import reblogApi from "api/reblogApi";
