a8a5765c7b740b18bae5306e2421d6704a2eacb6
Arjun Lubana <arjunlubana562@gmail.com>
2022-06-10 12:42:59 +0300

    Change color of Toolbar

--- src/components/Toolbar/ToolbarButton.jsx
+++ src/components/Toolbar/ToolbarButton.jsx
@@ -1,9 +1,14 @@
 import Button from "@mui/material/Button";
 
 export default function ToolbarButton({ icon, toggleStyle }) {
-	return (
-		<Button variant="outlined" onClick={() => toggleStyle()} sx={{height: 50}} >
-			{icon}
-		</Button>
-	);
+  return (
+    <Button
+      variant="contained"
+      color="secondary"
+      onClick={() => toggleStyle()}
+      sx={{ height: 40, p: 2, borderRadius: 0, display: "flex", outline: "1px solid #ead6db" }}
+    >
+      {icon}
+    </Button>
+  );
 }
