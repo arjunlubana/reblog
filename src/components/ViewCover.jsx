f1ef5fc1f6fb43a2de368d26b2b34dccc7466901
Arjun Lubana <arjunlubana562@gmail.com>
2022-07-21 05:03:47 +0300

    Remove unused console log

--- src/components/ViewCover.jsx
+++ src/components/ViewCover.jsx
@@ -9,7 +9,6 @@ import { useCloudinary } from "hooks";
 
 export default function ViewCover() {
   const imageStore = useCloudinary();
-  console.log(imageStore);
   const img = imageStore.image("Reblog/cover_1656483998268_197473747");
   return (
     <AdvancedImage
