8ce4d1aa19bda8b1b904c74c955bfa5964fa2219
Arjun Lubana <arjunlubana562@gmail.com>
2022-08-02 06:23:05 +0300

    Show use avatar in the nav bar

--- src/components/NavBar/NavAvatar.jsx
+++ src/components/NavBar/NavAvatar.jsx
@@ -1,7 +1,15 @@
 import { Avatar } from "@mui/material";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function NavAvatar() {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
   return (
-    <Avatar src="./assets/profile.jpg" sx={{ outline: "2px solid #ead6db" }} />
+    isAuthenticated && (
+      <Avatar src={user.picture} sx={{ outline: "2px solid #ead6db" }} />
+    )
   );
 }
